4cfd754b28610d4f6ac47cf01861baa260f346d1
Lingao Jin <jinlingao@outlook.com>
2022-06-22 20:11:51 +0800

    refact: modify option page

--- options/index.tsx
+++ options/index.tsx
@@ -1,15 +1,12 @@
 import { useState } from "react"
 
 function OptionsIndex() {
-  const [data, setData] = useState("")
 
   return (
     <div>
       <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+        Option page
       </h1>
-      <h2>This is the Option UI page!</h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
     </div>
   )
 }
