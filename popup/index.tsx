ba1c5e225ead9383a67f41f2fc68bdbf96082872
Lingao Jin <jinlingao@outlook.com>
2022-06-24 11:13:41 +0800

    feat: add desc in popup

--- popup/index.tsx
+++ popup/index.tsx
@@ -19,6 +19,11 @@ function IndexPopup() {
         }}>
         星际老男孩论坛插件
       </h1>
+      <ul>
+        <li>自动签到</li>
+        <li>显示未读消息</li>
+        <li>跳转上次阅读位置</li>
+      </ul>
     </div>
   )
 }
